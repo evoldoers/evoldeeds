c35b06aea97fef3782af9adc6fda4a5538b6027f
Ian Holmes <ihholmes@gmail.com>
2024-05-16 20:46:08 +0300

    Improve likelihood.py

--- likelihood.py
+++ likelihood.py
@@ -4,45 +4,42 @@ from jax.scipy.linalg import expm
 
 # Compute substitution log-likelihood of a multiple sequence alignment and phylogenetic tree by pruning
 # Parameters:
-#  - alignment: (N,L) integer tokens. L is the length of the alignment (#cols), N is the number of sequences (#rows). A token of -1 indicates a gap.
-#  - distanceToParent: (N,) floats, distance to parent node
-#  - parentIndex: (N,) integers, index of parent node. Nodes are sorted in preorder so parentIndex[i] < i for all i. parentIndex[0] = -1
-#  - subRate: (A,A) substitution rate matrix
-#  - rootFreq: (A,) root frequencies (typically equilibrium frequencies for substitution rate matrix)
+#  - alignment: (R,C) integer tokens. C is the length of the alignment (#cols), R is the number of sequences (#rows). A token of -1 indicates a gap.
+#  - distanceToParent: (R,) floats, distance to parent node
+#  - parentIndex: (R,) integers, index of parent node. Nodes are sorted in preorder so parentIndex[i] <= i for all i. parentIndex[0] = -1
+#  - subRate: (*H,A,A) substitution rate matrix/matrices. Leading H axes (if any) are "hidden" substitution rate categories, A is alphabet size
+#  - rootFreq: (*H,A) root frequencies (typically equilibrium frequencies for substitution rate matrix)
+# To pad rows, set parentIndex[paddingRow:] = arange(paddingRow,R) and (for numerical stability) distanceToParent[paddingRow:] = 0
+# To pad columns, set alignment[paddingRow,paddingCol:] = -1
 
 def pruneLogLike (alignment, distanceToParent, parentIndex, subRate, rootFreq):
     assert alignment.ndim == 2
-    assert subRate.ndim == 2
-    N, L = alignment.shape
-    A = subRate.shape[0]
-    assert distanceToParent.shape == (N,)
-    assert parentIndex.shape == (N,)
-    assert rootFreq.shape == (A,)
-    assert subRate.shape == (A,A)
+    assert subRate.ndim >= 2
+    R, C = alignment.shape
+    *H, A = subRate.shape[0:-1]
+    assert distanceToParent.shape == (R,)
+    assert parentIndex.shape == (R,)
+    assert rootFreq.shape == (*H,A)
+    assert subRate.shape == (*H,A,A)
     assert alignment.dtype == jnp.int32
     assert parentIndex.dtype == jnp.int32
     assert jnp.all(alignment >= -1)
     assert jnp.all(alignment < A)
     assert jnp.all(distanceToParent >= 0)
     assert jnp.all(parentIndex[1:] >= -1)
-    assert jnp.all(parentIndex < jnp.arange(N))
-    # Ensure that the substitution rate matrix is normalized
-    subRate = jnp.maximum(subRate, 0)
-    subRate = subRate - jnp.diag(jnp.sum(subRate, axis=1))
-    rootFreq = jnp.maximum(rootFreq, 0)
-    rootFreq = rootFreq / jnp.sum(rootFreq)
+    assert jnp.all(parentIndex <= jnp.arange(R))
     # Compute transition matrices per branch
-    subMatrix = expm (jnp.einsum('ij,n->nij', subRate, distanceToParent))
+    subMatrix = expm (jnp.einsum('...ij,r->...rij', subRate, distanceToParent))  # (*H,R,A,A)
     # Initialize pruning matrix
     tokenLookup = jnp.concatenate([jnp.ones(A)[None,:],jnp.eye(A)])
-    likelihood = tokenLookup[alignment + 1]  # (N,L,A)
-    logNorm = jnp.zeros(L)
+    likelihood = jnp.repeat (jnp.expand_dims (tokenLookup[alignment + 1], jnp.arange(len(H))), repeats=H, axis=0)  # (*H,R,C,A)
+    logNorm = jnp.zeros(*H,C)  # (*H,C)
     # Compute log-likelihood for all columns in parallel by iterating over nodes in postorder
     for child in range(A-1,0,-1):
         parent = parentIndex[child]
-        likelihood = likelihood.at[parent,:,:].multiply (jnp.einsum('ij,lj->li', subMatrix[child], likelihood[child,:,:]))
-        maxLike = jnp.max(likelihood[parent,:,:], axis=-1)  # (L,)
-        likelihood = likelihood.at[parent,:,:].divide (maxLike[:,None])
+        likelihood = likelihood.at[...,parent,:,:].multiply (jnp.einsum('...ij,...cj->...ci', subMatrix[...,child,:,:], likelihood[...,child,:,:]))
+        maxLike = jnp.max(likelihood[...,parent,:,:], axis=-1)  # (*H,C)
+        likelihood = likelihood.at[...,parent,:,:].divide (maxLike[...,None])
         logNorm = logNorm + jnp.log(maxLike)
-    logNorm = logNorm + jnp.log(jnp.einsum('li,i->l', likelihood.at[0,:,:], rootFreq))  # (L,)
+    logNorm = logNorm + jnp.log(jnp.einsum('...ci,...i->...c', likelihood.at[:,0,:,:], rootFreq))  # (*H,C)
     return logNorm
