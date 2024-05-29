fdf35c8cf1a14450f0f0826c89ff07ff717ffbe1
Ian Holmes <ihholmes@gmail.com>
2024-05-29 07:25:32 -0700

    chattier optimizer progress messages

--- python/optimize.py
+++ python/optimize.py
@@ -25,8 +25,14 @@ def optimize_generic (take_step, params, prefix="Iteration ", max_iter=1000, min
     last_loss = None
     for iter in range(max_iter):
         next_params, loss = take_step (params, iter)
-        inc = (best_loss - loss) / abs(best_loss) if best_loss is not None else 1
-        change_desc = "first" if best_loss is None else "better" if inc >= min_inc else "negligibly better" if loss < best_loss else "same" if loss == best_loss else "less worse" if loss < last_loss else "worse"
+        inc = (best_loss - loss) / abs(best_loss) if best_loss is not None else 2*min_inc
+        if inc >= min_inc:
+            patience_counter = 0
+        else:
+            patience_counter += 1
+        change_desc = "first" if best_loss is None else "better" if inc >= min_inc else "underwhelming" if loss < best_loss else "same" if loss == best_loss else "stalled" if loss == last_loss else "rallying" if loss < last_loss else "worse"
+        if patience_counter >= patience/2:
+            change_desc += "; stopping" + ((" in %d" % (patience - patience_counter)) if patience_counter < patience else "")
         if verbose:
             logging.warning ("%s%d: loss %f (%s)" % (prefix,iter+1,loss,change_desc))
         if best_loss is None or loss < best_loss:
@@ -34,12 +40,8 @@ def optimize_generic (take_step, params, prefix="Iteration ", max_iter=1000, min
             best_loss = loss
         params = next_params
         last_loss = loss
-        if inc >= min_inc:
-            patience_counter = 0
-        else:
-            patience_counter += 1
-            if patience_counter >= patience:
-                break
+        if patience_counter >= patience:
+            break
     if verbose:
         logging.warning ("%s%d: best loss %f" % (prefix,iter+1,best_loss))
     return best_params, best_loss
