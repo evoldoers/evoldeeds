018a03b67ec987503693afc7e590c912e262fc9c
Ian Holmes <ihholmes@gmail.com>
2024-06-11 17:02:31 +0100

    tinkering with CTBNs

--- python/ctbn.py
+++ python/ctbn.py
@@ -40,6 +40,18 @@ def q_bar_cond (i, C, S, J, h, mu):
     mu_exp_JC = jnp.einsum ('kx,jkyx->jky', mu, exp_minus_2JC)  # (j,k,y_i)
     return S[None,None,:,:] * jnp.exp(-h)[None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,None,:] * product(mu_exp_JC,axis=-2)[:,:,None,:]  # (j,x_j,x_i,y_i)
 
+# Returns (K,N,N) matrix where entry (j,x_j,x_i) is the mean-field averaged negative exit rate from x_i, conditioned on component #j being in state x_j
+def q_bar_cond_diag (i, C, S, J, h, mu):
+    K = mu.shape[0]
+    cond_mask = jnp.ones((K,K)) - jnp.eye(K)  # (j,k)
+    cond_energy = C[i,:,None,None] * J[None,:,:]  # (j,x_j,y_i)
+    Ci_mask = jnp.einsum ('jk,k->jk', cond_mask, C[i,:])  # (j,k)
+    exp_minus_2J = jnp.exp (-2 * J)  # (N,N)    
+    exp_minus_2JC = exp_minus_2J[None,None,:,:] ** Ci_mask[:,:,None,None]  # (j,k,y_i,x_k)
+    mu_exp_JC = jnp.einsum ('kx,jkyx->jky', mu, exp_minus_2JC)  # (j,k,y_i)
+    return jnp.diag(S)[None,None,:] * jnp.exp(-h)[None,None,:] * jnp.exp(-2*cond_energy) * product(mu_exp_JC,axis=-2)  # (j,x_j,x_i)
+
+
 # Geometrically-averaged mean-field rates for a continuous-time Bayesian network
 # Returns (N,N) matrix
 def q_tilde (i, C, S, J, h, mu):
@@ -47,13 +59,17 @@ def q_tilde (i, C, S, J, h, mu):
     return S * jnp.exp(-h-2*mean_energy)[None,:]  # (x_i,y_i)
 
 # Returns (K,N,N,N) matrix where entry (j,x_j,x_i,y_i) is the geometrically-averaged rate x_i->y_i, conditioned on component #j being in state x_j
-def q_tilde_cond_xj (i, j, C, S, J, h, mu):
+def q_tilde_cond (i, C, S, J, h, mu):
     K = mu.shape[0]
     cond_mask = jnp.ones((K,K)) - jnp.eye(K)  # (j,k)
     cond_energy = C[i,:,None,None] * J[None,:,:]  # (j,x_j,y_i)
     Ci_mask = jnp.einsum ('jk,k->jk', cond_mask, C[i,:])  # (j,k)
     mean_energy = jnp.einsum ('kx,jk,yx->jy', mu, Ci_mask, J)  # (j,y_i)
     return S[None,None,:,:] * jnp.exp(-h)[None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,None,:] * jnp.exp(-2*mean_energy)[:,None,None,:]  # (j,x_j,x_i,y_i)
+
+# gamma
+def gamma (i, mu, rho, C, S, J, h):
+    return jnp.einsum ('x,xy,y,x->xy', mu[i,:], q_bar(i,C,S,J,h,mu), rho[i,:], 1/mu[i,:])
 
 
 # Calculate the variational bound for a continuous-time Bayesian network
