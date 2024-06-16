07e949a38fc2e715ee156cfcef8bc3610a52aeff
Ian Holmes <ihholmes@gmail.com>
2024-06-16 08:26:25 +0100

    first draft of CTBN component likelihood

--- python/ctbn.py
+++ python/ctbn.py
@@ -79,6 +79,8 @@ def q_tilde_cond (C, S, J, h, mu):
     return S[None,None,None,:,:] * jnp.exp(-h)[None,None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,:,None,:] * jnp.exp(-2*mean_energy)[:,:,None,None,:]  # (i,j,x_j,x_i,y_i)
 
 # Rate matrix for a single component, q_{xy} = S_{xy}
+# S: (N,N)
+# h: (N,)
 def q_single (S, h):
     N = S.shape[0]
     return S * offdiag_mask(N) * jnp.exp(-h)[None,:]
@@ -145,14 +147,100 @@ class SingleComponentPosteriorMu (SingleComponentPosteriorRho):
         mu = exp_qt * rho / self.exp_qT
         return mu
 
-# Calculate the variational bound for a continuous-time Bayesian network
-def ctbnVariationalBound (xs, ys, T, C, S, J, h):
+# helper to evaluate mu and rho from arrays of Solution-like objects
+def eval_mu_rho (mu_solns, rho_solns, t):
+    mu = jnp.stack ([mu_soln.evaluate(t) for mu_soln in mu_solns])
+    rho = jnp.stack ([rho_soln.evaluate(t) for rho_soln in rho_solns])
+    return mu, rho
+
+# wrappers for diffrax
+def F_term (t, F_t, args):
+    mu_solns, rho_solns, C, S, J, h = args
+    mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
+    return F_deriv (mu, rho, C, S, J, h)
+
+def solve_F (T, mu_solns, rho_solns, C, S, J, h, rtol=1e-3, atol=1e-6):
+    term = diffrax.ODETerm (F_term)
+    solver = diffrax.Dopri5()
+    controller = diffrax.PIDController (rtol=rtol, atol=atol)
+    return diffrax.diffeqsolve (term, solver, t0=0, t1=T, dt0=None, y0=0,
+                                args=(mu_solns, rho_solns, C, S, J, h),
+                                stepsize_controller = controller)
+
+def make_rho_term (i):
+    def rho_term (t, rho_i_t, args):
+        mu_solns, rho_solns, C, S, J, h = args
+        mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
+        rho = rho.at(i).set(rho_i_t)
+        return rho_deriv (i, mu, rho, C, S, J, h)
+    return rho_term
+
+def solve_rho (T, i, rho_i_T, mu_solns, rho_solns, C, S, J, h, rtol=1e-3, atol=1e-6):
+    N = S.shape[0]
+    term = diffrax.ODETerm (make_rho_term(i))
+    solver = diffrax.Dopri5()
+    controller = diffrax.PIDController (rtol=rtol, atol=atol)
+    return diffrax.diffeqsolve (term, solver, t0=T, t1=0, dt0=None, y0 = rho_i_T,
+                                args=(mu_solns, rho_solns, C, S, J, h),
+                                stepsize_controller = controller,
+                                saveat = diffrax.SaveAt(dense=True))
+
+def make_mu_term (i):
+    def mu_term (t, mu_i_t, args):
+        mu_solns, rho_solns, C, S, J, h = args
+        mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
+        mu = mu.at(i).set(mu_i_t)
+        return mu_deriv (i, mu, rho, C, S, J, h)
+    return mu_term
+
+def solve_mu (T, i, mu_i_0, mu_solns, rho_solns, C, S, J, h, rtol=1e-3, atol=1e-6):
+    term = diffrax.ODETerm (make_mu_term(i))
+    solver = diffrax.Dopri5()
+    controller = diffrax.PIDController (rtol=rtol, atol=atol)
+    return diffrax.diffeqsolve (term, solver, t0=0, t1=T, dt0=None, y0=mu_i_0,
+                                args=(mu_solns, rho_solns, C, S, J, h),
+                                stepsize_controller = controller,
+                                saveat = diffrax.SaveAt(dense=True))
+
+
+# Calculate the variational likelihood for a component in a continuous-time Bayesian network, conditioned on its Markov blanket
+def ctbnComponentLikelihood (xs, ys, T, C, S, J, h, mininc=1e-3):
+    K = C.shape[0]
+    N = S.shape[0]
     # initialize component-indexed arrays of mu, rho solutions, using single-component posteriors
+    q = q_single (S, h)
+    rho_solns = [SingleComponentPosteriorRho (q, T, xs[i], ys[i]) for i in range(K)]
+    mu_solns = [SingleComponentPosteriorMu (q, T, xs[i], ys[i]) for i in range(K)]
     # initialize F_current as variational bound for initial mu and rho, and F_prev as -Infinity
-    # create an array of boundary conditions for rho(T), with component #0 having unconstrained rho(T)
+    F_prev = -jnp.inf
+    F_current = solve_F (T, mu_solns, rho_solns, C, S, J, h)
+    # create arrays of boundary conditions for mu(0) and rho(T), with component #0 having unconstrained rho(T)
+    mu_0 = jnp.stack ([jax.nn.one_hot(x,N) for x in xs])
+    rho_T = jnp.stack ([jnp.ones(N)] + [jax.nn.one_hot(y,N) for y in ys[1:]])
     # while (F_current - F_prev)/F_prev > minimum relative increase:
     #  loop over component indices i:
     #   solve rho and then mu for component i, using diffrax, and replace single-component posteriors with diffrax Solution's
     #   F_prev <- F_current, F_current <- new variational bound
-    # return mu^0_y(T), i.e. the variational probability that component #0 is in state y at time T
-    pass
+    def while_cond_fun (args):
+        F_prev, F_current, rho_solns, mu_solns, best_result = args
+        return F_current > F_prev and (F_current - F_prev) / F_prev > mininc
+    def while_body_fun (args):
+        F_prev, F_current, rho_solns, mu_solns, best_result = args
+        F_prev = F_current
+        F_current = solve_F (T, mu_solns, rho_solns, C, S, J, h)
+        rho_solns, mu_solns = jax.lax.fori_loop (0, N, loop_body_fun, (rho_solns, mu_solns))
+        best_result = jax.lax.cond (F_current > F_prev, get_result(mu_solns), best_result)
+        return F_prev, F_current, rho_solns, mu_solns, best_result
+    def loop_body_fun (i, args):
+        rho_solns, mu_solns = args
+        i = N - 1 - i  # finish with component #0
+        rho_i = solve_rho (T, i, rho_T[i,:], mu_solns, rho_solns, C, S, J, h)
+        rho_solns = [jax.lax.cond(k==i, rho_i, rho_solns[k]) for k in range(N)]
+        mu_i = solve_mu (T, i, mu_0[i,:], mu_solns, rho_solns, C, S, J, h)
+        mu_solns = [jax.lax.cond(k==i, mu_i, mu_solns[k]) for k in range(N)]
+        return rho_solns, mu_solns
+    def get_result (mu_solns):
+        return mu_solns[0].evaluate(T)[ys[0]]
+    F_prev, F_current, rho_solns, mu_solns, best_result = jax.lax.while_loop (while_cond_fun, while_body_fun, (F_prev, F_current, rho_solns, mu_solns, get_result(mu_solns)))
+    # return mu^0_{y_0}}(T), i.e. the variational probability that component #0 is in state y_0 at time T
+    return best_result
