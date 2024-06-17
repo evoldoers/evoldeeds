fe781eb03064d4f14c61f736b031ec3b9f457e49
Ian Holmes <ihholmes@gmail.com>
2024-06-17 22:59:11 +0100

    wip refactor

--- python/ctbn.py
+++ python/ctbn.py
@@ -31,6 +31,10 @@ def rowNormalise (matrix):
 # J = symmetric coupling matrix (N*N). For i!=j, J_ij=J_ji=interaction strength between components i and j. J_ii = 0
 # h = bias vector (N). h_i=bias of state i
 
+# Since C is a sparse matrix, with each component having at most M<<K neighbors, we will represent it compactly as follows
+# nbr_idx = sparse neighbor matrix (M*L). nbr_idx[i,n] is the index of the n-th neighbor of component i
+# nbr_mask = sparse neighbor flag matrix (M*L). nbr_mask[i,n] is 1 if nbr_idx[i,n] is a real neighbor, 0 otherwise
+
 def normalise_ctbn_params (params):
     return { 'S' : symmetrise(rowNormalise(params['S'])),
              'J' : symmetrise(params['J']),
@@ -44,66 +48,66 @@ def normalise_ctbn_params (params):
 # i = 1..K
 # x = (K,) vector of integers from 0..N-1
 # y_i = integer from 0..N-1
-def q_k (i, x, y_i, C, params):
+def q_k (i, x, y_i, nbr_idx, nbr_mask, params):
     S = params['S']
     J = params['J']
     h = params['h']
-    return S[x[i],y_i] * jnp.exp (-h[y_i] - 2*jnp.dot (C[i,:], J[y_i,x]))
+    return S[x[i],y_i] * jnp.exp (-h[y_i] - 2*jnp.dot (nbr_mask[i], J[y_i,x[nbr_idx[i]]]))
 
 # Mean-field averaged rates for a continuous-time Bayesian network
-# Returns (K,N,N) matrix where entry (i,x_i,y_i) is mean-field averaged rate matrix for component #i
-def q_bar (C, params, mu):
-    K, N = mu.shape
+# Returns (A,N,N) matrix where entry (a,x_{idx[a]},y_{idx[a]}) is mean-field averaged rate matrix for component idx[a]
+def q_bar (idx, nbr_idx, nbr_mask, params, mu):
+    N = mu.shape[-1]
     S = params['S']
     J = params['J']
     h = params['h']
     exp_minus_2J = jnp.exp (-2 * J)  # (y_i,x_k)
-    exp_minus_2JC = exp_minus_2J[None,None,:,:] ** C[:,:,None,None]  # (i,k,y_i,x_k)
-    mu_exp_JC = jnp.einsum ('kx,ikyx->iky', mu, exp_minus_2JC)  # (i,k,y_i)
+    exp_minus_2JC = exp_minus_2J[None,None,:,:] ** nbr_mask[idx,:,None,None]  # (a,k,y_i,x_{nbr_k})
+    mu_exp_2JC = jnp.einsum ('akx,kyx->aky', mu[nbr_idx[idx]], exp_minus_2JC)  # (a,k,y_i)
     S = S * offdiag_mask(N)
-    return S[None,:,:] * jnp.exp(-h)[None,None,:] * product(mu_exp_JC,axis=-2,keepdims=True)  # (i,x_i,y_i)
+    return S[None,:,:] * jnp.exp(-h)[None,None,:] * product(mu_exp_2JC,axis=-2,keepdims=True)  # (a,x_i,y_i)
 
-# Returns (K,K,N,N,N) tensor where entry (i,j,x_j,x_i,y_i) is the mean-field averaged rate x_i->y_i, conditioned on component #j being in state x_j
+# Returns (M,N,N,N) tensor where entry (j,x_j,x_i,y_i) is the mean-field averaged rate x_i->y_i, conditioned on component nbr_idx[i,j] being in state x_{nbr_idx[i,j]}
 # NB only valid for x_i != y_i
-def q_bar_cond (C, params, mu):
-    K, N = mu.shape
+def q_bar_cond (i, nbr_idx, nbr_mask, params, mu):
+    M = nbr_idx.shape[-1]
+    N = mu.shape[-1]
     S = params['S']
     J = params['J']
     h = params['h']
-    nonself_mask = offdiag_mask(K)  # (x,y)
-    cond_energy = C[:,:,None,None] * J[None,:,:]  # (i,j,x_j,y_i)
-    Ci_mask = jnp.einsum ('jk,ik->ijk', nonself_mask, C)  # (i,j,k)
+    nonself_nbr_mask = offdiag_mask(M) * jnp.outer(nbr_mask[i],nbr_mask[i])  # (j,k)
+    cond_energy = nbr_mask[i,:,None,None] * J[None,:,:]  # (j,x_{nbr_j},y_i)
     exp_minus_2J = jnp.exp (-2 * J)  # (N,N)
-    exp_minus_2JC = exp_minus_2J[None,None,None,:,:] ** Ci_mask[:,:,None,None,None]  # (i,j,k,y_i,x_k)
-    mu_exp_JC = jnp.einsum ('kx,ijkyx->ijky', mu, exp_minus_2JC)  # (i,j,k,y_i)
+    exp_minus_2JC = exp_minus_2J[None,None,:,:] ** nonself_nbr_mask[:,:,None,None]  # (j,k,y_i,x_{nbr_k})
+    mu_exp_JC = jnp.einsum ('kx,jkyx->jky', mu[nbr_idx[i]], exp_minus_2JC)  # (j,k,y_i)
     S = S * offdiag_mask(N)
-    return S[None,None,None,:,:] * jnp.exp(-h)[None,None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,:,None,:] * product(mu_exp_JC,axis=-2)[:,:,:,None,:]  # (i,j,x_j,x_i,y_i)
+    return S[None,None,:,:] * jnp.exp(-h)[None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,None,:] * product(mu_exp_JC,axis=-2)[:,None,None,:]  # (j,x_{nbr_j},x_i,y_i)
 
 # Geometrically-averaged mean-field rates for a continuous-time Bayesian network
-# Returns (K,N,N) matrix where entry (i,x_i,y_i) is geometrically-averaged mean-field rate matrix for component #i
+# Returns (A,N,N) matrix where entry (a,x_{idx[a]},y_{idx[a]}) is geometrically-averaged mean-field rate matrix for component idx[a]
 # NB only valid for x_i != y_i
-def q_tilde (C, params, mu):
-    K, N = mu.shape
+def q_tilde (idx, nbr_idx, nbr_mask, params, mu):
+    N = mu.shape[-1]
     S = params['S']
     J = params['J']
     h = params['h']
-    mean_energy = jnp.einsum ('kx,ik,yx->iy', mu, C, J)  # (i,y_i)
+    mean_energy = jnp.einsum ('akx,ak,yx->ay', mu[nbr_idx[idx,:]], nbr_mask[idx,:], J)  # (a,y_i)
     S = S * offdiag_mask(N)
-    return S[None,:,:] * jnp.exp(-h-2*mean_energy)[None,None,:]  # (i,x_i,y_i)
+    return S[None,:,:] * jnp.exp(-h-2*mean_energy)[:,None,:]  # (a,x_i,y_i)
 
-# Returns (K,K,N,N,N) matrix where entry (i,j,x_j,x_i,y_i) is the geometrically-averaged rate x_i->y_i, conditioned on component #j being in state x_j
+# Returns (M,N,N,N) matrix where entry (j,x_j,x_i,y_i) is the geometrically-averaged rate x_i->y_i, conditioned on component nbr_idx[i,j] being in state x_{nbr_idx[i,j]}
 # NB only valid for x_i != y_i
-def q_tilde_cond (C, params, mu):
-    K, N = mu.shape
+def q_tilde_cond (i, nbr_idx, nbr_mask, params, mu):
+    M = nbr_idx.shape[-1]
+    N = mu.shape[-1]
     S = params['S']
     J = params['J']
     h = params['h']
-    nonself_mask = offdiag_mask(K)  # (j,k)
-    cond_energy = C[:,:,None,None] * J[None,None,:,:]  # (i,j,x_j,y_i)
-    Ci_mask = jnp.einsum ('jk,ik->ijk', nonself_mask, C)  # (i,j,k)
-    mean_energy = jnp.einsum ('kx,ijk,yx->ijy', mu, Ci_mask, J)  # (i,j,y_i)
+    nonself_nbr_mask = offdiag_mask(M) * jnp.outer(nbr_mask[i],nbr_mask[i])  # (j,k)
+    cond_energy = nbr_mask[i,:,None,None] * J[None,:,:]  # (j,x_{nbr_j},y_i)
+    mean_energy = jnp.einsum ('kx,jk,yx->jy', mu[nbr_idx[i]], nonself_nbr_mask, J)  # (j,y_i)
     S = S * offdiag_mask(N)
-    return S[None,None,None,:,:] * jnp.exp(-h)[None,None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,:,None,:] * jnp.exp(-2*mean_energy)[:,:,None,None,:]  # (i,j,x_j,x_i,y_i)
+    return S[None,None,:,:] * jnp.exp(-h)[None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,None,:] * jnp.exp(-2*mean_energy)[:,None,None,:]  # (j,x_{nbr_j},x_i,y_i)
 
 # Rate matrix for a single component, q_{xy} = S_{xy}
 # S: (N,N)
@@ -116,52 +120,53 @@ def q_single (params):
 
 # Amalgamated (joint) rate matrix for all components
 # Note this is big: (N^K,N^K)
-def q_joint (C, params):
+def q_joint (nbr_idx, nbr_mask, params):
     N = params['S'].shape[0]
-    K = C.shape[0]
+    K,M = nbr_idx.shape
     def get_components (x):
         return jnp.array ([(x//(N**j))%N for j in range(K)])
     def get_rate (xs, ys):
         diffs = jnp.where (xs == ys, 0, 1)
         i = jnp.argmax (diffs)
-        return jnp.where (jnp.sum(diffs) == 1, q_k(i, xs, ys[i], C, params), 0)
+        return jnp.where (jnp.sum(diffs) == 1, q_k(i, xs, ys[i], nbr_idx, nbr_mask, params), 0)
     states = jax.vmap (get_components)(jnp.arange(N**K))
     Q = jax.vmap (lambda x: jax.vmap (lambda y: get_rate(x,y))(states))(states)
     return rowNormalise(Q)
 
 # gamma
-# Returns (K,N,N) matrix where entry (i,x_i,y_i) is the joint probability of transition x_i->y_i for component #i
-def gamma (mu, rho, C, params):
-    return jnp.einsum ('ix,ixy,iy,iy->ixy', mu, q_tilde(C,params,mu), rho, 1/mu)
+# Returns (A,N,N) matrix where entry (k,x_{idx[a]},y_{idx[a]}) is the joint probability of transition x_{idx[a]}->y_{idx[a]} for component idx[a]
+def gamma (idx, nbr_idx, nbr_mask, params, mu, rho):
+    return jnp.einsum ('ax,axy,ay,ay->axy', mu[idx], q_tilde(idx,nbr_idx,nbr_mask,params,mu), rho[idx], 1/mu[idx])
 
-# Returns (K,N) matrix
-def psi (mu, rho, C, params):
-    _gamma = gamma(mu,rho,C,params)  # (K,N,N)
-    qbar_cond = q_bar_cond(C,params,mu)  # (K,K,N,N,N)
-    qtilde_cond = q_tilde_cond(C,params,mu)  # (K,K,N,N,N)
-    log_qtilde_cond = -safe_log (jnp.where (qtilde_cond < 0, 1, qtilde_cond))  # (K,K,N,N,N)
-    return jnp.einsum('jy,jixyz->ix',mu,qbar_cond) + jnp.einsum('jxy,jixyz->ix',_gamma,log_qtilde_cond)
+# Returns (N,) vector
+def psi (i, nbr_idx, nbr_mask, params, mu, rho):
+    gammas = gamma(nbr_idx[i], nbr_idx, nbr_mask, params, mu, rho)  # (M,N,N)
+    qbar_cond = q_bar_cond(i,nbr_idx,nbr_mask,params,mu)  # (M,N,N,N)
+    qtilde_cond = q_tilde_cond(i,nbr_idx,nbr_mask,params,mu)  # (M,N,N,N)
+    log_qtilde_cond = -safe_log (jnp.where (qtilde_cond < 0, 1, qtilde_cond))  # (M,N,N,N)
+    return jnp.einsum('jy,jxyz,j->x',mu[nbr_idx[i]],qbar_cond,nbr_mask[i]) + jnp.einsum('jxy,jxyz,j->x',gammas,log_qtilde_cond,nbr_mask[i])
 
-def rho_deriv (i, mu, rho, C, params):
+def rho_deriv (i, nbr_idx, nbr_mask, params, mu, rho):
     K = mu.shape[0]
-    qbar = q_bar(C,params,mu)  # (K,N,N)
-    qbar_diag = jnp.einsum ('kxy->kx', qbar)  # (K,N)
-    _psi = psi(mu,rho,C,params)  # (K,N)
-    qtilde = q_tilde(C,params,mu)  # (K,N,N)
-    rho_deriv_i = -rho[i,:] * (qbar_diag[i,:] + _psi[i,:]) - jnp.einsum ('y,xy->x', rho[i,:], qtilde[i,:,:])  # (N,)
+    qbar = q_bar(jnp.array([i]), nbr_idx, nbr_mask, params, mu)[0,:,:]  # (N,N)
+    qbar_diag = jnp.einsum ('xy->x', qbar)  # (N,)
+    _psi = psi(i, nbr_idx, nbr_mask, params, mu, rho)  # (N,)
+    qtilde = q_tilde(jnp.array([i]), nbr_idx, nbr_mask, params, mu)  # (1,N,N)
+    rho_deriv_i = -rho[i,:] * (qbar_diag + _psi) - jnp.einsum ('y,xy->x', rho[i,:], qtilde[0,:,:])  # (N,)
     return rho_deriv_i
 
-def mu_deriv (i, mu, rho, C, params):
+def mu_deriv (i, nbr_idx, nbr_mask, params, mu, rho):
     K = mu.shape[0]
-    _gamma = gamma(mu,rho,C,params)  # (K,N,N)
-    mu_deriv_i = jnp.einsum('yx->x',_gamma[i,:]) - jnp.einsum('xy->y',_gamma[i,:])  # (N,)
+    _gamma = gamma(jnp.array([i]), nbr_idx, nbr_mask, params, mu, rho)[0,:,:]  # (N,N)
+    mu_deriv_i = jnp.einsum('yx->x',_gamma) - jnp.einsum('xy->y',_gamma)  # (N,)
     return mu_deriv_i
 
-def F_deriv (mu, rho, C, params):
-    N = mu.shape[-1]
-    qbar = q_bar(C,params,mu)  # (K,N,N)
-    qtilde = q_tilde(C,params,mu)  # (K,N,N)
-    _gamma = gamma (mu, rho, C, params)  # (K,N,N)
+def F_deriv (nbr_idx, nbr_mask, params, mu, rho):
+    K, N = mu.shape
+    idx = jnp.arange(K)
+    qbar = q_bar(idx, nbr_idx, nbr_mask, params, mu)  # (K,N,N)
+    qtilde = q_tilde(idx, nbr_idx, nbr_mask, params, mu)  # (K,N,N)
+    _gamma = gamma (idx, nbr_idx, nbr_mask, params, mu, rho)  # (K,N,N)
     mask = offdiag_mask(N)[None,:,:]  # (1,N,N)
     log_qtilde = safe_log(jnp.where(mask,qtilde,1))
     gamma_coeff = log_qtilde + 1 + safe_log(mu)[:,:,None] - safe_log(_gamma)
@@ -211,12 +216,14 @@ def solve_F (T, mu_solns, rho_solns, C, params, rtol=1e-3, atol=1e-6):
                                 args=(mu_solns, rho_solns, C, params),
                                 stepsize_controller = controller)
 
+# TODO: refactor from here
+
 def make_rho_term (i):
     def rho_term (t, rho_i_t, args):
         mu_solns, rho_solns, C, params = args
         mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
         rho = rho.at(i).set(rho_i_t)
-        return rho_deriv (i, mu, rho, C, params)
+        return rho_deriv (i, nbr_idx, nbr_mask, params, mu, rho)
     return rho_term
 
 def solve_rho (T, i, rho_i_T, mu_solns, rho_solns, C, params, rtol=1e-3, atol=1e-6):
@@ -234,7 +241,7 @@ def make_mu_term (i):
         mu_solns, rho_solns, C, params = args
         mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
         mu = mu.at(i).set(mu_i_t)
-        return mu_deriv (i, mu, rho, C, params)
+        return mu_deriv (i, nbr_idx, nbr_mask, params, mu, rho)
     return mu_term
 
 def solve_mu (T, i, mu_i_0, mu_solns, rho_solns, C, params, rtol=1e-3, atol=1e-6):
