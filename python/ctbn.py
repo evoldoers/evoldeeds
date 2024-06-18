f8c5a26b143232b46a42780d17790c3f2d7d29aa
Ian Holmes <ihholmes@gmail.com>
2024-06-18 09:26:00 +0100

    refactor to use variational likelihood for whole sequence, with sparse contact matrix. flip sign of J and h.

--- python/ctbn.py
+++ python/ctbn.py
@@ -17,9 +17,17 @@ def safe_log (x):
 def symmetrise (matrix):
     return (matrix + matrix.swapaxes(-1,-2)) / 2
 
-def rowNormalise (matrix):
+def row_normalise (matrix):
     matrix = jnp.abs (matrix)
     return matrix - jnp.diag(jnp.sum(matrix, axis=-1))
+
+def round_up_to_power (x, base=2):
+    if base == 2:  # avoid doubling length due to precision errors
+        x = 1 << (len-1).bit_length()
+    else:
+        x = int (jnp.ceil (base ** jnp.ceil (jnp.log(len) / jnp.log(base))))
+    return x
+
 
 # Implements the algorithm from Cohn et al (2010), for protein Potts models parameterized by contact & coupling matrices
 # Cohn et al (2010), JMLR 11:93. Mean Field Variational Approximation for Continuous-Time Bayesian Networks
@@ -36,7 +44,7 @@ def rowNormalise (matrix):
 # nbr_mask = sparse neighbor flag matrix (M*L). nbr_mask[i,n] is 1 if nbr_idx[i,n] is a real neighbor, 0 otherwise
 
 def normalise_ctbn_params (params):
-    return { 'S' : symmetrise(rowNormalise(params['S'])),
+    return { 'S' : symmetrise(row_normalise(params['S'])),
              'J' : symmetrise(params['J']),
              'h' : params['h'] }
 
@@ -61,11 +69,11 @@ def q_bar (idx, nbr_idx, nbr_mask, params, mu):
     S = params['S']
     J = params['J']
     h = params['h']
-    exp_minus_2J = jnp.exp (-2 * J)  # (y_i,x_k)
-    exp_minus_2JC = exp_minus_2J[None,None,:,:] ** nbr_mask[idx,:,None,None]  # (a,k,y_i,x_{nbr_k})
-    mu_exp_2JC = jnp.einsum ('akx,kyx->aky', mu[nbr_idx[idx]], exp_minus_2JC)  # (a,k,y_i)
+    exp_2J = jnp.exp (2 * J)  # (y_i,x_k)
+    exp_2JC = exp_2J[None,None,:,:] ** nbr_mask[idx,:,None,None]  # (a,k,y_i,x_{nbr_k})
+    mu_exp_2JC = jnp.einsum ('akx,kyx->aky', mu[nbr_idx[idx]], exp_2JC)  # (a,k,y_i)
     S = S * offdiag_mask(N)
-    return S[None,:,:] * jnp.exp(-h)[None,None,:] * product(mu_exp_2JC,axis=-2,keepdims=True)  # (a,x_i,y_i)
+    return S[None,:,:] * jnp.exp(h)[None,None,:] * product(mu_exp_2JC,axis=-2,keepdims=True)  # (a,x_i,y_i)
 
 # Returns (M,N,N,N) tensor where entry (j,x_j,x_i,y_i) is the mean-field averaged rate x_i->y_i, conditioned on component nbr_idx[i,j] being in state x_{nbr_idx[i,j]}
 # NB only valid for x_i != y_i
@@ -77,11 +85,11 @@ def q_bar_cond (i, nbr_idx, nbr_mask, params, mu):
     h = params['h']
     nonself_nbr_mask = offdiag_mask(M) * jnp.outer(nbr_mask[i],nbr_mask[i])  # (j,k)
     cond_energy = nbr_mask[i,:,None,None] * J[None,:,:]  # (j,x_{nbr_j},y_i)
-    exp_minus_2J = jnp.exp (-2 * J)  # (N,N)
-    exp_minus_2JC = exp_minus_2J[None,None,:,:] ** nonself_nbr_mask[:,:,None,None]  # (j,k,y_i,x_{nbr_k})
-    mu_exp_JC = jnp.einsum ('kx,jkyx->jky', mu[nbr_idx[i]], exp_minus_2JC)  # (j,k,y_i)
+    exp_2J = jnp.exp (2 * J)  # (N,N)
+    exp_2JC = exp_2J[None,None,:,:] ** nonself_nbr_mask[:,:,None,None]  # (j,k,y_i,x_{nbr_k})
+    mu_exp_JC = jnp.einsum ('kx,jkyx->jky', mu[nbr_idx[i]], exp_2JC)  # (j,k,y_i)
     S = S * offdiag_mask(N)
-    return S[None,None,:,:] * jnp.exp(-h)[None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,None,:] * product(mu_exp_JC,axis=-2)[:,None,None,:]  # (j,x_{nbr_j},x_i,y_i)
+    return S[None,None,:,:] * jnp.exp(h)[None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,None,:] * product(mu_exp_JC,axis=-2)[:,None,None,:]  # (j,x_{nbr_j},x_i,y_i)
 
 # Geometrically-averaged mean-field rates for a continuous-time Bayesian network
 # Returns (A,N,N) matrix where entry (a,x_{idx[a]},y_{idx[a]}) is geometrically-averaged mean-field rate matrix for component idx[a]
@@ -93,7 +101,7 @@ def q_tilde (idx, nbr_idx, nbr_mask, params, mu):
     h = params['h']
     mean_energy = jnp.einsum ('akx,ak,yx->ay', mu[nbr_idx[idx,:]], nbr_mask[idx,:], J)  # (a,y_i)
     S = S * offdiag_mask(N)
-    return S[None,:,:] * jnp.exp(-h-2*mean_energy)[:,None,:]  # (a,x_i,y_i)
+    return S[None,:,:] * jnp.exp(h+2*mean_energy)[:,None,:]  # (a,x_i,y_i)
 
 # Returns (M,N,N,N) matrix where entry (j,x_j,x_i,y_i) is the geometrically-averaged rate x_i->y_i, conditioned on component nbr_idx[i,j] being in state x_{nbr_idx[i,j]}
 # NB only valid for x_i != y_i
@@ -107,7 +115,7 @@ def q_tilde_cond (i, nbr_idx, nbr_mask, params, mu):
     cond_energy = nbr_mask[i,:,None,None] * J[None,:,:]  # (j,x_{nbr_j},y_i)
     mean_energy = jnp.einsum ('kx,jk,yx->jy', mu[nbr_idx[i]], nonself_nbr_mask, J)  # (j,y_i)
     S = S * offdiag_mask(N)
-    return S[None,None,:,:] * jnp.exp(-h)[None,None,None,:] * jnp.exp(-2*cond_energy)[:,:,None,:] * jnp.exp(-2*mean_energy)[:,None,None,:]  # (j,x_{nbr_j},x_i,y_i)
+    return S[None,None,:,:] * jnp.exp(h)[None,None,None,:] * jnp.exp(2*cond_energy)[:,:,None,:] * jnp.exp(2*mean_energy)[:,None,None,:]  # (j,x_{nbr_j},x_i,y_i)
 
 # Rate matrix for a single component, q_{xy} = S_{xy}
 # S: (N,N)
@@ -116,7 +124,7 @@ def q_single (params):
     S = params['S']
     h = params['h']
     N = S.shape[0]
-    return S * offdiag_mask(N) * jnp.exp(-h)[None,:]
+    return S * offdiag_mask(N) * jnp.exp(h)[None,:]
 
 # Amalgamated (joint) rate matrix for all components
 # Note this is big: (N^K,N^K)
@@ -131,7 +139,7 @@ def q_joint (nbr_idx, nbr_mask, params):
         return jnp.where (jnp.sum(diffs) == 1, q_k(i, xs, ys[i], nbr_idx, nbr_mask, params), 0)
     states = jax.vmap (get_components)(jnp.arange(N**K))
     Q = jax.vmap (lambda x: jax.vmap (lambda y: get_rate(x,y))(states))(states)
-    return rowNormalise(Q)
+    return row_normalise(Q)
 
 # gamma
 # Returns (A,N,N) matrix where entry (k,x_{idx[a]},y_{idx[a]}) is the joint probability of transition x_{idx[a]}->y_{idx[a]} for component idx[a]
@@ -161,13 +169,13 @@ def mu_deriv (i, nbr_idx, nbr_mask, params, mu, rho):
     mu_deriv_i = jnp.einsum('yx->x',_gamma) - jnp.einsum('xy->y',_gamma)  # (N,)
     return mu_deriv_i
 
-def F_deriv (nbr_idx, nbr_mask, params, mu, rho):
+def F_deriv (seq_mask, nbr_idx, nbr_mask, params, mu, rho):
     K, N = mu.shape
     idx = jnp.arange(K)
     qbar = q_bar(idx, nbr_idx, nbr_mask, params, mu)  # (K,N,N)
     qtilde = q_tilde(idx, nbr_idx, nbr_mask, params, mu)  # (K,N,N)
     _gamma = gamma (idx, nbr_idx, nbr_mask, params, mu, rho)  # (K,N,N)
-    mask = offdiag_mask(N)[None,:,:]  # (1,N,N)
+    mask = seq_mask[:,None,None] * offdiag_mask(N)[None,:,:]  # (K,N,N)
     log_qtilde = safe_log(jnp.where(mask,qtilde,1))
     gamma_coeff = log_qtilde + 1 + safe_log(mu)[:,:,None] - safe_log(_gamma)
     dF = -jnp.einsum('ix,ixy,ixy->',mu,qbar,mask) + jnp.einsum('ixy,ixy,ixy->',_gamma,gamma_coeff,mask)
@@ -204,64 +212,57 @@ def eval_mu_rho (mu_solns, rho_solns, t):
 
 # wrappers for diffrax
 def F_term (t, F_t, args):
-    mu_solns, rho_solns, C, params = args
+    seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns = args
     mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
-    return F_deriv (mu, rho, C, params)
+    return F_deriv (seq_mask, nbr_idx, nbr_mask, params, mu, rho)
 
-def solve_F (T, mu_solns, rho_solns, C, params, rtol=1e-3, atol=1e-6):
+def solve_F (seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns, T, rtol=1e-3, atol=1e-6):
     term = diffrax.ODETerm (F_term)
     solver = diffrax.Dopri5()
     controller = diffrax.PIDController (rtol=rtol, atol=atol)
     return diffrax.diffeqsolve (term, solver, t0=0, t1=T, dt0=None, y0=0,
-                                args=(mu_solns, rho_solns, C, params),
+                                args=(seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns),
                                 stepsize_controller = controller)
 
-# TODO: refactor from here
-
-def make_rho_term (i):
+def make_rho_term (i, seq_mask):
     def rho_term (t, rho_i_t, args):
-        mu_solns, rho_solns, C, params = args
+        nbr_idx, nbr_mask, params, mu_solns, rho_solns = args
         mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
         rho = rho.at(i).set(rho_i_t)
-        return rho_deriv (i, nbr_idx, nbr_mask, params, mu, rho)
+        return seq_mask[i] * rho_deriv (i, nbr_idx, nbr_mask, params, mu, rho)
     return rho_term
 
-def solve_rho (T, i, rho_i_T, mu_solns, rho_solns, C, params, rtol=1e-3, atol=1e-6):
+def solve_rho (i, rho_i_T, seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns, T, rtol=1e-3, atol=1e-6):
     N = params['S'].shape[0]
-    term = diffrax.ODETerm (make_rho_term(i))
+    term = diffrax.ODETerm (make_rho_term(i, seq_mask))
     solver = diffrax.Dopri5()
     controller = diffrax.PIDController (rtol=rtol, atol=atol)
     return diffrax.diffeqsolve (term, solver, t0=T, t1=0, dt0=None, y0 = rho_i_T,
-                                args=(mu_solns, rho_solns, C, params),
+                                args=(nbr_idx, nbr_mask, params, mu_solns, rho_solns),
                                 stepsize_controller = controller,
                                 saveat = diffrax.SaveAt(dense=True))
 
-def make_mu_term (i):
+def make_mu_term (i, seq_mask):
     def mu_term (t, mu_i_t, args):
-        mu_solns, rho_solns, C, params = args
+        nbr_idx, nbr_mask, params, mu_solns, rho_solns = args
         mu, rho = eval_mu_rho (mu_solns, rho_solns, t)
         mu = mu.at(i).set(mu_i_t)
-        return mu_deriv (i, nbr_idx, nbr_mask, params, mu, rho)
+        return seq_mask[i] * mu_deriv (i, nbr_idx, nbr_mask, params, mu, rho)
     return mu_term
 
-def solve_mu (T, i, mu_i_0, mu_solns, rho_solns, C, params, rtol=1e-3, atol=1e-6):
-    term = diffrax.ODETerm (make_mu_term(i))
+def solve_mu (i, mu_i_0, seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns, T, rtol=1e-3, atol=1e-6):
+    term = diffrax.ODETerm (make_mu_term(i, seq_mask))
     solver = diffrax.Dopri5()
     controller = diffrax.PIDController (rtol=rtol, atol=atol)
     return diffrax.diffeqsolve (term, solver, t0=0, t1=T, dt0=None, y0=mu_i_0,
-                                args=(mu_solns, rho_solns, C, params),
+                                args=(nbr_idx, nbr_mask, params, mu_solns, rho_solns),
                                 stepsize_controller = controller,
                                 saveat = diffrax.SaveAt(dense=True))
 
-# Calculate the variational likelihood for a component in a continuous-time Bayesian network, conditioned on its Markov blanket
-def ctbn_component_probability (xs, ys, T, C, params, **kwargs):
-    ys_neg0 = ys.at(0).set(-1)
-    F, rho_solns, mu_solns = ctbn_variational_fit (xs, ys_neg0, T, C, params, **kwargs)
-    return mu_solns[0].evaluate(T)[ys[0]]
-
-# Calculate the best variational fit for an endpoint-conditioned continuous-time Bayesian network
-def ctbn_variational_fit (xs, ys, T, C, params, min_inc=1e-3, max_updates=3):
-    K = C.shape[0]
+# Calculate the variational likelihood for an endpoint-conditioned continuous-time Bayesian network
+# This is a strict lower bound for log P(X_T=ys|X_0=xs,T,params)
+def ctbn_variational_fit (xs, ys, seq_mask, nbr_idx, nbr_mask, params, T, min_inc=1e-3, max_updates=3):
+    K = nbr_idx.shape[0]
     N = params['S'].shape[0]
     params = normalise_ctbn_params (params)
     # initialize component-indexed arrays of mu, rho solutions, using single-component posteriors
@@ -270,11 +271,10 @@ def ctbn_variational_fit (xs, ys, T, C, params, min_inc=1e-3, max_updates=3):
     mu_solns = [ExactMu (q, T, xs[i], ys[i]) for i in range(K)]
     # initialize F_current as variational bound for initial mu and rho, and F_prev as -Infinity
     F_prev = -jnp.inf
-    F_current = solve_F (T, mu_solns, rho_solns, C, params)
-    # create arrays of boundary conditions for mu(0) and rho(T), with y==-1 implying unconstrained rho(T)
-    ones_or_onehot = jnp.concatenate([jnp.ones(N)[None,:],jnp.eye(N)])
+    F_current = solve_F (seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns, T)
+    # create arrays of boundary conditions for mu(0) and rho(T)
     mu_0 = jnp.eye(N)[xs]
-    rho_T = ones_or_onehot[ys + 1]
+    rho_T = jnp.eye(N)[ys]
     # while (F_current - F_prev)/F_prev > minimum relative increase:
     #  loop over component indices i:
     #   solve rho and then mu for component i, using diffrax, and replace single-component posteriors with diffrax Solution's
@@ -285,44 +285,42 @@ def ctbn_variational_fit (xs, ys, T, C, params, min_inc=1e-3, max_updates=3):
     def while_body_fun (args):
         F_prev, (F_current, rho_solns, mu_solns), (F_best, rho_best, mu_best), n_updates = args
         F_prev = F_best
-        F_current = solve_F (T, mu_solns, rho_solns, C, params)
+        F_current = solve_F (seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns, T)
         rho_solns, mu_solns = jax.lax.fori_loop (0, N, loop_body_fun, (rho_solns, mu_solns))
-        keep_best = lambda: (F_best, rho_best, mu_best)
+        old_best = lambda: (F_best, rho_best, mu_best)
         new_best = lambda: (F_current, rho_solns, mu_solns)
-        F_best, rho_best, mu_best = jax.lax.cond (F_current > F_best, new_best, keep_best)
+        F_best, rho_best, mu_best = jax.lax.cond (F_current > F_best, new_best, old_best)
         return F_prev, (F_current, rho_solns, mu_solns), (F_best, rho_best, mu_best), n_updates + 1
     def loop_body_fun (i, args):
         rho_solns, mu_solns = args
         i = N - 1 - i  # finish with component #0
-        new_rho_i = solve_rho (T, i, rho_T[i,:], mu_solns, rho_solns, C, params)
+        new_rho_i = solve_rho (i, rho_T[i,:], seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns, T)
         rho_solns = [new_rho_i if k==i else old_rho_i for k, old_rho_i in enumerate(rho_solns)]
-        new_mu_i = solve_mu (T, i, mu_0[i,:], mu_solns, rho_solns, C, params)
+        new_mu_i = solve_mu (i, mu_0[i,:], seq_mask, nbr_idx, nbr_mask, params, mu_solns, rho_solns, T)
         mu_solns = [new_mu_i if k==i else old_mu_i for k, old_mu_i in enumerate(mu_solns)]
         return rho_solns, mu_solns
     init_state = F_current, rho_solns, mu_solns
     F_prev, _final_state, best_state = jax.lax.while_loop (while_cond_fun, while_body_fun, (F_prev, init_state, init_state, 0))
     return best_state
 
-# Given a contact matrix C for a sequence, return a site-indexed list of tuples:
-# - the Markov blanket for site i, plus site i itself (i.e. a list of sites j!=i such that C[i,j] != 0)
-# - the contact matrix for that list of sites (i.e. i plus its Markov blanket)
-def get_Markov_blankets (C):
-    N = C.shape[0]
-    C_with_self = jnp.abs(C) + jnp.eye(N)  # has nonzero wherever C does, plus the diagonal
-    def get_blanket (i):
-        blanket = C_with_self[i,:].nonzero()
-        return blanket, C[blanket,:][:,blanket]
-    return [get_blanket(i) for i in range(N)]
-
-# Given two aligned (ungapped) sequences, a contact matrix,
-# and a site mask (which can be used to mask out sites that are added for padding reasons),
-# along with the parameters of a Potts continuous-time Bayes network substitution model,
-# return the log of the pseudolikelihood for descendant (sequence #2) given ancestor (sequence #1)
-def ctbn_log_pseudolikelihood (xseq, yseq, mask, T, C, params):
-    pl_terms = [ctbn_component_probability (xseq[blanket], yseq[blanket], T, C_local, params)
-                for (blanket, C_local) in get_Markov_blankets(C)]
-    log_pl = jnp.sum (jnp.where(mask, jnp.log(pl_terms), 0))
-    return log_pl
+# Given sequences xs,ys and contact matrix C, return padded xs,ys along with seq_mask,nbr_idx,nbr_mask
+def get_Markov_blankets (xs, ys, C, K=None, M=None):
+    K_prepad = len(xs)
+    assert len(ys) == K_prepad, "xs and ys must have the same length"
+    assert C.shape == (K_prepad,K_prepad), "C must be a square matrix of the same size as xs"
+    if K is None:
+        K = round_up_to_power (K_prepad)
+    nbr_idx_list = [C[i,:].nonzero() for i in range(K_prepad)]
+    if M is None:
+        M = round_up_to_power (max ([len(nbr_idx) for nbr_idx in nbr_idx_list]))
+    else:
+        assert M >= max ([len(nbr_idx) for nbr_idx in nbr_idx_list]), "M must be at least as large as the largest number of neighbors"
+    seq_mask = jnp.array ([i < K_prepad for i in range(K)])
+    nbr_mask = jnp.array ([[1] * len(nbr_idx) + [0] * (M - len(nbr_idx)) for nbr_idx in nbr_idx_list] + [[0] * M] * (K - K_prepad))
+    nbr_idx = jnp.array ([nbr_idx + [0] * (M - len(nbr_idx)) for nbr_idx in nbr_idx_list])
+    xs = xs + [0] * (K - K_prepad)
+    ys = ys + [0] * (K - K_prepad)
+    return xs, ys, seq_mask, nbr_idx, nbr_mask
 
 # Weak L2 regularizer for J and h
 def param_regularizer (params, alpha=1e-4):
--- python/ctbn_test.py
+++ python/ctbn_test.py
@@ -4,8 +4,8 @@ import ctbn
 # Tests
 # For the two-component chain, use Example 1 from Cohn et al (2010)
 def ising2 (q_same = 10, q_diff = 1):
-    J_same = -jnp.log(q_same) / 2
-    J_diff = -jnp.log(q_diff) / 2
+    J_same = jnp.log(q_same) / 2
+    J_diff = jnp.log(q_diff) / 2
     C = jnp.ones((2,2)) - jnp.eye(2)
     S = jnp.array ([[0, 1], [1, 0]])
     J = jnp.array ([[J_same, J_diff], [J_diff, J_same]])
@@ -18,7 +18,7 @@ def telegraph (K = 1, lambda1 = 1, lambda2 = 2):
     C = jnp.zeros((K, K))
     S = jnp.array ([[0, 1], [1, 0]])
     J = jnp.zeros((2,2))
-    h = -jnp.log(jnp.array([lambda2, lambda1]))  # lambda2 is the rate from state #2 -> #1, h[0] is the bias of state #1
+    h = jnp.log(jnp.array([lambda2, lambda1]))  # lambda2 is the rate from state #2 -> #1, h[0] is the bias of state #1
     params = { 'S': S, 'J': J, 'h': h }
     return C, params
 
